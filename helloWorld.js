65992e435e3591ab71b3324683123b5dc3bdfcfa
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-10-28 21:49:44 -0400

    add js code

---
+++ helloWorld.js
@@ -0,0 +1,8 @@
+// Print "Hello World!" to the screen
+class Solution{
+  
+    //creating a function main for point so it can print out hello world
+    static main(){
+    console.log("Hello World!")
+      }
+  }
